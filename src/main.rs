1eeb4cef788005e0d4eac16b8c5021442ac48f80
BalaBoi <sanjay.krishnan456@gmail.com>
2024-10-07 02:35:01 +0530

    add database to state

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 use zer02prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
@@ -6,5 +7,10 @@ async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let database_connection = PgConnection::connect(
+            &config.database_settings.connection_string()
+        )
+        .await
+        .expect("Couldn't get database connection");
+    run(listener, database_connection)?.await
 }

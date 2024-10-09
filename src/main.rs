4332c64dc28b905fc2c1bd9a76b958fe3a977078
BalaBoi <sanjay.krishnan456@gmail.com>
2024-10-10 05:02:21 +0530

    Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
-    let database_pool = PgPool::connect(&config.database_settings.connection_string().expose_secret())
-        .await
+    let database_pool = PgPool::connect_lazy(&config.database_settings.connection_string().expose_secret())
         .expect("Couldn't get database connection");
     run(listener, database_pool)?.await
 }
